b41f4717ab475bc3639a9d2f856569f2b2b697ec
kushal-1 <homakattel1@gmail.com>
2020-10-13 08:49:46 +0545

    Changes added

--- EmployeeMgmt/Controllers/EmployeeController.cs
+++ EmployeeMgmt/Controllers/EmployeeController.cs
@@ -6,29 +6,24 @@ public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-         Person empl1 = new Person()
-        {
-            FirstName = "Utsav",
-            SurName = "Kuinkel",
-            Address = "Mulpani",
-            Gender = 'M',
-            Salary = 100.0,
-        };
- 
-        Person empl2 = new Person() { FirstName = "Utsav", SurName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl3 = new Person() { FirstName = "Utsav", SurName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl4 = new Person() { FirstName = "Utsav", SurName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl5 = new Person() { FirstName = "Utsav", SurName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl6 = new Person() { FirstName = "Utsav", SurName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl7 = new Person() { FirstName = "Utsav", SurName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        Person empl8 = new Person() { FirstName = "Utsav", SurName = "Kuinkel", Address = "Mulpani", Gender = 'M', Salary = 100.0 };
-        List<Person> employees = new List<Person>() { empl1, empl2, empl3, empl4, empl5, empl6, empl7, empl8 };
-        return View(employees);
+        List<Person> persons = Person.GetEmployee();
+        return View(persons);
 
     }
 
-   
-
+       public ActionResult Detail(string firstName){
+        List<Person> persons = Person.GetEmployee();
+        Person p1 = null;
+        foreach(var p in persons){
+            if (p.FirstName == firstName){
+                p1 = p;
+            }
+        }
+        if (p1 != null){
+            return View(p1);
+        }
+        return View();
+    }
 }
 
 
--- EmployeeMgmt/Models/Person.cs
+++ EmployeeMgmt/Models/Person.cs
@@ -1,5 +1,18 @@
+using System.Collections.Generic;
+
 public class Person
 {
+     public Person(string firstName, string lastName,string address, char gender, double salary,string nickName,object hobbies,object whoCanBeFriends) {
+        firstName = FirstName;
+        lastName = SurName;
+        address = Address;
+        gender = Gender;
+        salary = Salary;
+        whoCanBeFriends= WhoCanBeFriends;
+        nickName= NickName;
+        hobbies= Hobbies;
+
+    }
     public string FirstName{ get; set; }
 
     public string SurName{ get; set; } 
@@ -10,5 +23,31 @@ public class Person
     
    
     public double Salary{ get; set; }
-      
+    public string NickName{get; set;}
+
+
+    public object Hobbies{get; set;}
+    public object WhoCanBeFriends{get; set;}
+  public static List<Person> GetEmployee(){
+         Person empl1 = new Person("Kushal","Kattel","Solukhumbu",'M',1000,"KOKAA",   "Playing guitar, Singing", "Loyal,Rich");
+ 
+        Person empl2 = new Person( "Rupesh",  "Chaulagain",  "Aakahsedhara",   'M',  100.0,"Rupu",
+        "Playing guitar, Dancing", "Loyal,Rich");
+        Person empl3 = new Person( "Aayam ", "Neupane",  "Hattiban",  'M',100.0,"Aalu",
+        "Playing guitar, Dancing", "Loyal,Rich" );
+        Person empl4 = new Person("Ayush",   "Karki",  "Kapan", 'M',  100.0,"Jungye",
+         "Playing guitar, Dancing",  "Loyal,Rich");
+        Person empl5 = new Person( "Paras",  "Tiwari",   "Boudha", 'M',   100.0,"Paru",
+        "Playing guitar, Dancing", "Loyal,Rich") ;
+        Person empl6 = new Person( "Utshav",  "Kuikel",   "Boudha", 'M',   100.0,"mitra",
+        "Playing guitar, Dancing", "Loyal,Rich");  
+        Person empl7 = new Person("Sampanna",  "Nepal",   "Chabahil", 'M',   100.0,"Cyan",
+        "Playing guitar, Dancing", "Loyal,Rich"); 
+        Person empl8 = new Person("Shreedhar",  "Pandey",   "Boudha", 'M',   100.0,"Pandey","Playing guitar, Dancing", "Loyal,Rich");
+
+        List<Person> persons = new List<Person>{ empl1, empl2, empl3, empl4, empl5, empl6, empl7, empl8 };
+        return persons;
+    }
+
+
 }
